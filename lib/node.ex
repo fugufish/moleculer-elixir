8823566e50a691a7ed2e1fbc7af4f76ea62a590c
jarod reid <therealfugu@gmail.com>
2021-06-12 23:08:45 -0600

    add node

---
+++ lib/node.ex
@@ -0,0 +1,32 @@
+defmodule Moleculer.Node do
+  @moduledoc """
+  Represents a Node on the Molecuer network.
+  """
+  use Agent
+
+  def start_link(node_spec) do
+    {:ok, name} = Map.fetch(node_spec, :sender)
+
+    Agent.start_link(fn -> node_spec end, name: String.to_atom("#{__MODULE__}.#{name}"))
+  end
+
+  @doc """
+  Returns the node's spec
+  """
+  def spec(name) do
+    Agent.get(module_name(name), fn node_spec -> node_spec end)
+  end
+
+  @doc """
+  Returns the node's service specs
+  """
+  def service_specs(name) do
+    {:ok, services} = Map.fetch(spec(name), :services)
+
+    services
+  end
+
+  defp module_name(name) do
+    String.to_atom("#{__MODULE__}.#{name}")
+  end
+end
