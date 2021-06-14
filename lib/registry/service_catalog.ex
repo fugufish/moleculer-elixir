6099945e65faf95aeed37a17a2adbc22fb4acf38
jarod reid <therealfugu@gmail.com>
2021-06-14 07:24:45 -0600

    update service catalog

--- lib/registry/service_catalog.ex
+++ lib/registry/service_catalog.ex
@@ -1,69 +1,15 @@
 defmodule Moleculer.Registry.ServiceCatalog do
   @moduledoc false
 
-  use Supervisor
+  use Moleculer.Registry.Catalog
 
-  def start_link(_) do
-    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
-  end
-
-  @impl true
-  def init(_) do
-    children = [
-      {Registry, [keys: :duplicate, name: __MODULE__.Registry]},
-      {DynamicSupervisor, [name: __MODULE__.ServiceSupervisor, strategy: :one_for_one]}
-    ]
-
-    Supervisor.init(children, strategy: :one_for_one)
-  end
-
-  @doc """
-  Adds a local service to the catalog.
-  """
-  @spec add(service :: Moleculer.Service) :: :ok
-  def add(service) do
-    add(service, remote: false)
-  end
+  alias Moleculer.Service
 
   @doc """
   Adds a new service to the catalog with the provided options.
   """
-  @spec add(service :: Moleculer.Service, options :: map()) :: :ok
-  def add(service, options) do
-    {:ok, pid} = DynamicSupervisor.start_child(__MODULE__.ServiceSupervisor, {service, options})
-
-    Registry.register(__MODULE__.Registry, service.name(), pid)
-
-    :ok
-  end
-
-  @doc """
-  Returns true if the service of the given name exists. Otherwise false.
-  """
-  @spec exists?(name :: atom()) :: boolean()
-  def exists?(name) do
-    entries = lookup(name)
-
-    Enum.count(entries) > 0
-  end
-
-  @doc """
-  Returns a list of `pid`s for the provided service name.
-  """
-  @spec lookup(name :: atom()) :: [{pid(), any()}]
-  def lookup(name) do
-    Registry.lookup(__MODULE__.Registry, name)
-  end
-
-  def lookup(name, remote: true) do
-    entries = lookup(name)
-
-    Enum.filter(entries, fn e -> send(elem(e, 0), :remote?) end)
-  end
-
-  def lookup(name, local: true) do
-    entries = lookup(name)
-
-    Enum.filter(entries, fn e -> send(elem(e, 0), :local?) end)
+  @spec add(service :: Service) :: :ok
+  def add(service) do
+    add(service, service.name(), %{})
   end
 end
--- test/moleculer/registry/service_catalog_test.exs
+++ test/moleculer/registry/service_catalog_test.exs
@@ -17,10 +17,7 @@ defmodule Moleculer.Registry.ServiceCatalogTest do
 
   describe "exists/1" do
     setup do
-      start_supervised!(%{
-        id: :"test-service",
-        start: {ServiceCatalog, :start_link, [%{}]}
-      })
+      start_supervised!({ServiceCatalog, []})
 
       :ok
     end
@@ -28,41 +25,7 @@ defmodule Moleculer.Registry.ServiceCatalogTest do
     test "it returns true when the service of the given name exists" do
       ServiceCatalog.add(TestService)
 
-      assert ServiceCatalog.exists?(:"test-service")
-    end
-  end
-
-  describe "lookup/2 when options.remote == true" do
-    setup do
-      start_supervised!(%{
-        id: :"test-service",
-        start: {ServiceCatalog, :start_link, [%{}]}
-      })
-
-      :ok
-    end
-
-    test "it returns the correct services" do
-      ServiceCatalog.add(TestService, remote: true)
-
-      services = ServiceCatalog.lookup(:"test-service", remote: true)
-    end
-  end
-
-  describe "lookup/2 when options.local == true" do
-    setup do
-      start_supervised!(%{
-        id: :"test-service",
-        start: {ServiceCatalog, :start_link, [%{}]}
-      })
-
-      :ok
-    end
-
-    test "it returns the correct services" do
-      ServiceCatalog.add(TestService, remote: false)
-
-      services = ServiceCatalog.lookup(:"test-service", local: true)
+      assert ServiceCatalog.lookup(:"test-service")
     end
   end
 end
